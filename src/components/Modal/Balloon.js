0f0e7b03fb62c78d22bb8bf39bc4b34eb75193f0
godcl1623 <delphi5281@gmail.com>
2021-08-20 01:51:36 +0900

    libmanage - develop - 풍선형 메뉴 구현 테스트

--- src/components/Modal/Balloon.js
+++ src/components/Modal/Balloon.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const Balloon = ({contents}) => (
-  <article id="balloon">
-    {contents}
+const Balloon = ({contents, display, style, hand}) => (
+  <article
+    id="balloon"
+    style={display}
+  >
+    <section
+      className="balloon-body"
+      style={style}
+    >
+      {contents}
+    </section>
+    <div
+      className="balloon-hand"
+      style={hand}
+    ></div>
   </article>
 );
 
