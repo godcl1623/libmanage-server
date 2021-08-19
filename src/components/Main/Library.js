f629d650003a23352bbeb7a6ab4495c2cf47436e
godcl1623 <delphi5281@gmail.com>
2021-08-20 01:59:35 +0900

    libmanage - develop - 풍선형 메뉴 구현 테스트: main의 header, library 모두 추가

--- src/components/Main/Library.js
+++ src/components/Main/Library.js
@@ -10,23 +10,67 @@ const Options = () => (
   </>
 );
 
-const Library = () => (
-  <article
-    id="library"
-    style={{
-      'flex': '2'
-    }}
-  >
-    <button>옵션</button>
-    <Balloon contents={<Options />} />
-    <ul id="contents-lists">
-      <li>라이브러리 1</li>
-      <li>라이브러리 2</li>
-      <li>라이브러리 3</li>
-      <li>라이브러리 4</li>
-      <li>라이브러리 5</li>
-    </ul>
-  </article>
-);
+const Library = () => {
+  const [balloonState, setBalloonState] = React.useState('none');
+
+  const wrapper = {
+    'display': balloonState,
+    'position': 'absolute',
+    'top': '50px',
+    'right': '0'
+  }
+
+  const style = {
+    'padding': '20px',
+    'display': balloonState,
+    'justify-content': 'center',
+    'align-items': 'center',
+    'width': '300px',
+    'height': '100px',
+    'position': 'absolute',
+    'top': '0',
+    'right': '0',
+    'background': 'white'
+  };
+
+  const hand = {
+    'width': '50px',
+    'height': '50px',
+    'position': 'absolute',
+    'top': '0',
+    'left': '0',
+    'transform': 'translate(-100%, -50%)',
+    'background': 'white',
+    'display': balloonState
+  }
+
+  return (
+    <article
+      id="library"
+      style={{
+        'flex': '2',
+        'position': 'relative'
+      }}
+    >
+      <button
+        onClick={() => {
+          if (balloonState === 'none') {
+            setBalloonState('flex');
+          } else {
+            setBalloonState('none');
+          }
+        }}
+      >옵션</button>
+      <Balloon contents={<Options />} display={wrapper} style={style} hand={hand} />
+      <ul id="contents-lists">
+        <li>라이브러리 1</li>
+        <li>라이브러리 2</li>
+        <li>라이브러리 3</li>
+        <li>라이브러리 4</li>
+        <li>라이브러리 5</li>
+      </ul>
+    </article>
+  );
+};
 
 export default Library;
