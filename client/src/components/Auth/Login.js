5b2e4385edd3b32cc92e572ade885bfbefe40f1b
godcl1623 <delphi5281@gmail.com>
2021-08-29 02:13:11 +0900

    프론트 -> 백 아이디, 비밀번호 전송

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,41 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 
-const Login = () => (
-  <article
-    id="login"
-    style={{
-      'display': 'flex',
-      'flexDirection': 'column',
-      'justifyContent': 'center',
-      'alignContent': 'center'
-    }}
-  >
-    <h2>libmanage</h2>
-    <form
-      action="/login_process"
+const Login = () => {
+  const [formData, setFormData] = React.useState({});
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setFormData(prevValue => ({
+      ...prevValue,
+      [name]: value
+    }));
+  };
+
+  return (
+    <article
+      id="login"
       style={{
         'display': 'flex',
         'flexDirection': 'column',
         'justifyContent': 'center',
         'alignContent': 'center'
       }}
-      onSubmit={e => {
-        e.preventDefault();
-        fetch()
-      }}
     >
-      <label htmlFor="ID">ID: </label>
-      <input type="text" name="ID" />
-      <label htmlFor="PWD">PW: </label>
-      <input type="password" name="PWD" />
-      <button type="submit" name="login">LOGIN</button>
-    </form>
-    <div className="member">
-      <button>회원가입</button>
-      <button>ID/PW 찾기</button>
-    </div>
-    <button>오프라인으로 접속</button>
-  </article>
-);
+      <h2>libmanage</h2>
+      <form
+        // action="/test"
+        style={{
+          'display': 'flex',
+          'flexDirection': 'column',
+          'justifyContent': 'center',
+          'alignContent': 'center'
+        }}
+        onSubmit={e => {
+          e.preventDefault();
+          axios.post('http://localhost:3002/test', formData)
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+          // axios.post('http://localhost:3002/test', loginInfo)
+          // .then(() => console.log('foo'))
+          // .catch(err => console.log(err));
+          // console.log(e.target.elements.ID.value)
+          // console.log(e.target.elements.PWD.value)
+        }}
+      >
+        <label htmlFor="ID">ID: </label>
+        <input type="text" name="ID" onChange={handleChange} />
+        <label htmlFor="PWD">PW: </label>
+        <input type="password" name="PWD" onChange={handleChange} />
+        <button type="submit" name="login">LOGIN</button>
+      </form>
+      <div className="member">
+        <button>회원가입</button>
+        <button>ID/PW 찾기</button>
+      </div>
+      <button>오프라인으로 접속</button>
+    </article>
+    );
+};
 
 export default Login;
--- server/core/login.js
+++ server/core/login.js
@@ -5,18 +5,22 @@ const bodyParser = require('body-parser');
 const servestatic = require('serve-static');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const cors = require('cors');
 
 const app = express();
 const port = 3002;
-const test = 'message sent from login server';
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 
 app.get('/', (req, res) => {
   res.send('login server');
 });
 
-app.get('/test', (req, res) => {
-  console.log(req);
-  console.log(res);
+app.post('/test', (req, res) => {
+  console.log(req.body);
+  // console.log(res);
 });
 
 app.listen(port, () => console.log(`server is running at port${port}`));
