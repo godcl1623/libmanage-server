11dcd65d03483d829809e9e3464c08474c6190be
godcl1623 <delphi5281@gmail.com>
2021-09-17 00:55:27 +0900

    libmanage - develop - steam web api: openid 테스트

--- server/core/apitest.js
+++ server/core/apitest.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
+const { Issuer } = require('openid-client');
+const { cyber, blade } = require('../custom_modules/security/fes');
 
-const getOwnedGames = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamId}&format=json`;
-const getPlayerSummaries = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamId}`;
+const getOwnedGames = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?include_appinfo=1&include_played_free_games=1&key=${cyber}&steamid=${blade}&format=json`;
+const getPlayerSummaries = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${cyber}&steamids=${blade}&format=json`;
 
-axios.get(getOwnedGames)
-  .then(res => console.log(res.data.response));
+// axios.get(getPlayerSummaries)
+//   .then(res => console.log(res.data.response.players));
+Issuer.discover('http://steamcommunity.com/openid')
+  .then(res => console.log(res))
--- server/custom_modules/utils/index.js
+++ server/custom_modules/utils/index.js
@@ -1,3 +1,10 @@
 module.exports = {
-  getRandom: () => String(Math.random()).split('.')[1].slice(0, 6)
+  getRandom: () => String(Math.random()).split('.')[1].slice(0, 6),
+  apiOptions: {
+    getOwned: {
+      appinfo: 'include_appinfo=1',
+      freeGame: 'include_played_free_games=1',
+      format: 'format=json'
+    }
+  }
 }
