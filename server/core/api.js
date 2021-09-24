4f3a2ec19b3844f0c7be8c3d9c7d26e0e3e846cb
godcl1623 <delphi5281@gmail.com>
2021-09-25 01:17:46 +0900

    libmanage - develop - 스팀 라이브러리를 통한 IGDB 검색 기능 개선: Promise를 이용해 한 라우터에서 모든 검색 담당

--- server/core/api.js
+++ server/core/api.js
@@ -77,11 +77,8 @@ app.get('/auth/steam/return',
     axios.get(getOwnedGames)
       .then(result => {
         const rawGamesData = result.data.response.games;
-        const tempArr = [];
-        rawGamesData.forEach(gameDataObj => {
-          tempArr.push(gameDataObj.appid)
-        });
-        const sortedTempArr = tempArr.sort((prev, next) => prev < next ? -1 : 1);
+        const steamResult = rawGamesData.map(gameDataObj => gameDataObj.appid);
+        const sortedTempArr = steamResult.sort((prev, next) => prev < next ? -1 : 1);
         // 정렬된 게임 목록을 변수 gameList로 업데이트
         gameList = sortedTempArr
         // console.log(gameList);
@@ -89,14 +86,15 @@ app.get('/auth/steam/return',
       .then(() => {
         axios.post(`http://localhost:${port}/api_test`, { execute: 'order66' })
           .then(result => {
-            axios.post(`http://localhost:${port}/db_test`, { test: result.data })
-              .then(result => {
-                axios.post(`http://localhost:${port}/db_test2`, { test: result.data })
-                  .then(result => {
-                    axios.post(`http://localhost:${port}/db_test3`, { test: result.data })
-                      .then(result => console.log(result));
-                  })
-              })
+            // axios.post(`http://localhost:${port}/db_test`, { test: result.data })
+            //   .then(result => {
+            //     axios.post(`http://localhost:${port}/db_test2`, { test: result.data })
+            //       .then(result => {
+            //         axios.post(`http://localhost:${port}/db_test3`, { test: result.data })
+            //           .then(result => console.log(result));
+            //       })
+            //   })
+            axios.post(`http://localhost:${port}/promise_test`, { test: result.data }).then(res => console.log(res))
           })
       })
       // .then(() => res.redirect('http://localhost:3000/main'));
@@ -233,6 +231,106 @@ app.post('/db_test3', (req, res) => {
       igdbIDSearch(igdbID);
     }, index * 300);
   });
+});
+
+app.post('/promise_test', (req, res) => {
+  const { cid, access_token: token } = req.body.test;
+  const client = igdb(cid, token);
+  // 1. 스팀 게임별 고유 id와 IGDB 사이트에 등록된 스팀 url 대조 함수 - IGDB 고유 게임 아이디 이용 예정
+  const steamURLSearchQuery = async steamAppId => {
+    const response = await client
+      .fields(['*'])
+      .where(`category = 13 & url = *"/${steamAppId}"`)
+      .request('/websites');
+    return response;
+  };
+  // 2. 스팀 url 중 1과 다른 형식의 게임들 검색 함수 - IGDB 고유 게임 아이디 이용 예정
+  const steamURLException = async steamAppId => {
+    const response = await client
+      .fields(['*'])
+      .where(`category = 13 & url = *"/${steamAppId}/"*`)
+      .request('/websites');
+    return response;
+  };
+  // 3. IGDB 아이디를 이용한 게임 메타데이터 검색 함수
+  const igdbIDSearch = async igdbID => {
+    const response = await client
+      .fields(['*'])
+      // .search('cyberpunk 2077')
+      .where(`id = ${igdbID}`)
+      .request('/games');
+    return response;
+  };
+  // 4. IGDB상 저장된 스팀 게임의 url을 기반으로 IGDB 고유 게임 아이디 반환
+  const firstFilter = (rawData, filterFunc) => new Promise((resolve, reject) => {
+    const temp = [];
+    const fail = [];
+    rawData.forEach((steamAppId, index) => {
+      setTimeout(() => {
+        filterFunc(steamAppId)
+          .then(result => {
+            if (result.data[0] === undefined) {
+              fail.push(steamAppId);
+            } else {
+              temp.push(result.data[0].game);
+              // 기능 완성 이후 삭제할 것
+              console.log(temp.length + fail.length);
+            }
+            if (temp.length + fail.length === rawData.length) {
+              resolve({ temp, fail });
+            }
+          });
+      }, index * 300);
+    });
+  });
+  // 5. 4에서 검색에 실패한 게임들 대상 IGDB 고유 게임 아이디 검색 함수
+  const secondFilter = (rawData, filterFunc) => new Promise((resolve, reject) => {
+    const { temp, fail } = rawData;
+    const secTemp = [];
+    const secFail = [];
+    if (fail[0] !== undefined) {
+      fail.forEach((steamAppId, index, thisArr) => {
+        setTimeout(() => {
+          filterFunc(steamAppId)
+            .then(result => {
+              if (result.data[0] === undefined && thisArr.includes(steamAppId)) {
+                secFail.push(steamAppId);
+              // eslint-disable-next-line no-else-return
+              } else {
+                secTemp.push(result.data[0].game);
+                // 기능 구현 이후에 삭제할 것
+                console.log(secTemp.length + secFail.length);
+              }
+              if (secTemp.length + secFail.length === thisArr.length) {
+                resolve(temp.concat(secTemp).sort((prev, next) => prev < next ? -1 : 1));
+              }
+            });
+        }, index * 300);
+      });
+    } else {
+      resolve(temp);
+    }
+  });
+  // 6. 4, 5에서 검색된 IGDB 고유 아이디를 통한 게임 메타데이터 검색 함수
+  const returnMeta = (rawData, filterFunc) => new Promise((resolve, reject) => {
+    const temp = [];
+    rawData.forEach((igdbID, index) => {
+      setTimeout(() => {
+        filterFunc(igdbID)
+          .then(result => {
+            temp.push(result.data[0]);
+            console.log(temp.length);
+            if (temp.length === rawData.length) resolve(temp);
+          });
+      }, index * 300);
+    });
+  });
+  firstFilter(gameList, steamURLSearchQuery)
+    .then(rawURLSearchResult => secondFilter(rawURLSearchResult, steamURLException))
+    .then(gamesInIGDB => returnMeta(gamesInIGDB, igdbIDSearch))
+    // 최종 메타데이터 목록
+    .then(igdbResult => console.log(igdbResult))
+    .catch(err => console.log(err));
 });
 
 app.listen(port, () => console.log(`server is running at port${port}`));
